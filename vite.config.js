1ea5ecdb4a4c9cf02675a36db6156a37cd9ae16a
Vishakh Surendran <148840145+vishakhsurendran@users.noreply.github.com>
2025-09-24 20:32:07 -0400

    Added vite.config.js for redirect logic

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        contact: resolve(__dirname, 'contact.html')
+      }
+    }
+  }
+})
